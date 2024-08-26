c1e649544d0b57b0935bcaeb42e2dbc4d61c25e8
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-08-26 14:34:05 -0600

    Initial Commit

---
+++ WakesMyEyes/WakesMyEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakesMyEyesApp.swift
+//  WakesMyEyes
+//
+//  Created by Niels Ouvrard on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WakesMyEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
